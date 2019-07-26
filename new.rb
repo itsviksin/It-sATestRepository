fb9a85e7258cb82a65264c8aac7e5e69a5d21c0f
Vikas Kumar <vikas.kumar@optus.com.au>
2019-07-26 13:07:52 +1000

    Cleanest Commit

---
+++ new.rb
@@ -0,0 +1,3 @@
+
+fdkjdfjdkjfsd  ifjdijfd
+fdkjfdkjfdkjfThis is new file where i am going to write new autoamtion code
--- new1.rb
+++ new1.rb
@@ -1,2 +1,4 @@
+
+fdfdfdfdkhjdfdjfh
 #Here is the new program for test test test
-# I am just editing this one please take a look
+dkfjdfdkjfkdthis is test session
